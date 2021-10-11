4974abacd3873bcf59f02aeb65a022d5327f1813
Yuri Shvetsov <shvetsov.yuri.new@gmail.com>
2021-10-11 18:16:29 +0500

    fix: preloading of images/icons

--- src/js/preloadImages/index.js
+++ src/js/preloadImages/index.js
@@ -1,20 +1,21 @@
 import images from './images.json';
 
-(function() {
-
+function preloadImages() {
   const imageNames = images.names
   const path = '../../assets/images/'
-  const template = document.createElement('template')
-
-  function addImg(path) {
+  const div = document.createElement('div')
+  
+  div.style = 'width: 0; height: 0; position: absolute; position:absolute; z-index:-1000; opacity:0; overflow: hidden;'
+  
+  function preload(url) {
     let img = new Image()
 
-    img.src = path
-    template.append(img)
+    img.src = url
+    div.append(img)
   }
 
-  imageNames.forEach(name => addImg(path + name))
-  document.body.append(template)
+  imageNames.forEach(name => preload(path + name))
+  document.body.append(div)
+}
 
-}())
-
+window.onload = preloadImages
